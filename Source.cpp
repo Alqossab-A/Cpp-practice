5bb0fdace2d4eebe9b446587d744e41e38a69689
Ahmed Al-Qossab <alqossaba@gmail.com>
2023-03-12 21:41:13 -0400

    comments and small refactor

--- Source.cpp
+++ Source.cpp
@@ -7,30 +7,46 @@ char getYesNo();
 
 void printResponse(char responeToPrint);
 
-int main() {
-	welcome();
+void askYesOrNoQuestion();
 
-	char answer = getYesNo();
-
-	printResponse(answer);
+int main() 
+{
+	// Asks user to enter y or n and returns response
+	askYesOrNoQuestion();
 
 	system("pause");
 }
 
-void welcome() {
+void welcome() 
+{
+	// Welcome the user to the progam
 	cout << "Welcome:\n";
 }
 
-char getYesNo() {
+char getYesNo() 
+{
+	// Ask the user yes or no?
 	cout << "Please answer: y or n. \n";
 
 	char response;
 
+	// Get user input
 	cin >> response;
 
 	return response;
 }
 
-void printResponse(char responseToPrint) {
+void printResponse(char responseToPrint) 
+{
+	// Print response to screen
 	cout << "Your answer was: " << responseToPrint << endl;
+}
+
+void askYesOrNoQuestion() 
+{
+	welcome();
+
+	char answer = getYesNo();
+
+	printResponse(answer);
 }
